f197ed7995c4457d46f2c30c5c8ae24725ca3bda
Student <example@example.com>
2019-10-15 13:35:21 -0700

    add ability to edit animals

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
 
 namespace AnimalShelter.Controllers
@@ -37,6 +38,20 @@ namespace AnimalShelter.Controllers
         {
             Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
             return View(thisAnimal);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Animal animal)
+        {
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
