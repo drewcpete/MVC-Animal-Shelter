c1770e99db78c1626a9bf455b21da2fe800ca8c8
Student <example@example.com>
2019-10-15 16:04:58 -0700

    sortable lists

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using System;
 
 namespace AnimalShelter.Controllers
 {
@@ -15,11 +16,38 @@ namespace AnimalShelter.Controllers
             _db = db;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string orderVar)
         {
+            System.Console.WriteLine(orderVar);
             List<Animal> model = _db.Animals.ToList();
-            List<Animal> sortedAnimals = model.OrderBy(x => x.Name).ToList();
-            return View(sortedAnimals);
+           
+            ViewBag.NameSortParm = String.IsNullOrEmpty(orderVar) ? "name_desc" : "";
+            ViewBag.AgeSortParm = orderVar == "Age" ? "age_desc" : "Age";
+            ViewBag.TypeSortParm = orderVar == "Type" ? "type_desc" : "Type";
+            
+            switch (orderVar)
+            {
+                case "name_desc":
+                    model = model.OrderByDescending(s => s.Name).ToList();
+                    break;
+                case "Age":
+                    model = model.OrderBy(s => s.Age).ToList();
+                    break;
+                case "age_desc":
+                    model = model.OrderByDescending(s => s.Age).ToList();
+                    break;
+                case "Type":
+                    model = model.OrderBy(s => s.PetType).ToList();
+                    break;
+                case "type_desc":
+                    model = model.OrderByDescending(s => s.PetType).ToList();
+                    break;
+                default:
+                    model = model.OrderBy(s => s.Name).ToList();
+                    break;
+            }
+            return View(model.ToList());
+
         }
 
         public ActionResult Create()
@@ -51,6 +79,21 @@ namespace AnimalShelter.Controllers
         public ActionResult Edit(Animal animal)
         {
             _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            _db.Animals.Remove(thisAnimal);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
